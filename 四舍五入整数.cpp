4185aa939f4c23a18e2923615ee2317bcfefe311
Antarcticaa <32089950+Antarcticaa@users.noreply.github.com>
2017-10-24 14:55:58 +0800

    practice

--- 四舍五入整数.cpp
+++ 四舍五入整数.cpp
@@ -3,8 +3,8 @@ main()
 {
 	double x;
 	int y;
-	printf("请输入一个小数:\n");
-	scanf("%lf",&x);
+	puts("请输入一个小数.");
+	printf("此小数为：");    scanf("%lf",&x);
 	y=x+0.5;
 	printf("四舍五入整数后的值为：%d\n",y);
 	return 0;
